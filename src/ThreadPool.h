840db54102af101abf82059d5070d8c0309991e1
weixr18 <weixr18@mails.tsinghua.edu.cn>
2020-11-11 15:05:41 +0800

    recover

--- src/ThreadPool.h
+++ src/ThreadPool.h
@@ -1,3 +1,14 @@
+/*
+==========================================================================
+* 类ThreadPool是本代码的核心类，类中自动维护线程池的创建和任务队列的派送
+* 其中的TaskFun是任务函数
+* 其中的TaskCallbackFun是回调函数
+*用法：定义一个ThreadPool变量，TaskFun函数和TaskCallbackFun回调函数，然后调用ThreadPool的QueueTaskItem()函数即可
+Author: TTGuoying
+Date: 2018/02/19 23:15
+==========================================================================
+*/
+
 #pragma once
 #include <winsock2.h>
 #include <Windows.h>
